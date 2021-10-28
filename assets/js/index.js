c90da3f189de360a0c76fa6fe9683f06b099be0c
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-10-28 09:59:11 +0300

    Add switch

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,3 +44,27 @@
 //   slider.currentIndex = slider.prev();
 //   updateView();
 // })
+
+
+const [openBtn, closeBtn]=document.querySelectorAll('button');
+
+const switchInnerText=({target})=>{
+  const openElem = target===closeBtn ? openBtn : closeBtn;
+  const closeElem = target===closeBtn ? closeBtn : openBtn;
+
+  const buffer = openElem.textContent;
+  openElem.textContent=closeElem.textContent;
+  closeElem.textContent=buffer;
+
+  closeElem.removeEventListener('mouseenter', switchInnerText);
+  openElem.addEventListener('mouseenter', switchInnerText);
+}
+const clickHandler=({target})=>{
+  console.log(target)
+}
+
+openBtn.addEventListener('mouseenter', switchInnerText, {once:true});
+closeBtn.addEventListener('mouseenter', switchInnerText);
+
+openBtn.addEventListener('click', clickHandler);
+closeBtn.addEventListener('click', clickHandler);
