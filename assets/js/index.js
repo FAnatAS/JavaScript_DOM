883ca486348013ed6fb10b091ff3578dda68d69b
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-10-29 09:10:13 +0300

    Data + immersion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 // function logMesssage(){
-//   consike.log('hi!')
+//   console.log('hi!')
 // }
 
 // const [button] = document.getElementsByTagName('button');
@@ -46,25 +46,88 @@
 // })
 
 
-const [openBtn, closeBtn]=document.querySelectorAll('button');
+// const [openBtn, closeBtn]=document.querySelectorAll('button');
 
-const switchInnerText=({target})=>{
-  const openElem = target===closeBtn ? openBtn : closeBtn;
-  const closeElem = target===closeBtn ? closeBtn : openBtn;
+// const switchInnerText=({target})=>{
+//   const openElem = target===closeBtn ? openBtn : closeBtn;
+//   const closeElem = target===closeBtn ? closeBtn : openBtn;
 
-  const buffer = openElem.textContent;
-  openElem.textContent=closeElem.textContent;
-  closeElem.textContent=buffer;
+//   const buffer = openElem.textContent;
+//   openElem.textContent=closeElem.textContent;
+//   closeElem.textContent=buffer;
 
-  closeElem.removeEventListener('mouseenter', switchInnerText);
-  openElem.addEventListener('mouseenter', switchInnerText);
-}
-const clickHandler=({target})=>{
-  console.log(target)
-}
+//   closeElem.removeEventListener('mouseenter', switchInnerText);
+//   openElem.addEventListener('mouseenter', switchInnerText);
+// }
+// const clickHandler=({target})=>{
+//   console.log(target)
+// }
 
-openBtn.addEventListener('mouseenter', switchInnerText, {once:true});
-closeBtn.addEventListener('mouseenter', switchInnerText);
+// openBtn.addEventListener('mouseenter', switchInnerText, {once:true});
+// closeBtn.addEventListener('mouseenter', switchInnerText);
 
-openBtn.addEventListener('click', clickHandler);
-closeBtn.addEventListener('click', clickHandler);
+// openBtn.addEventListener('click', clickHandler);
+// closeBtn.addEventListener('click', clickHandler);
+
+// const btns = document.querySelectorAll('button');
+
+// //console.log(btns);
+// const btnHandler = ({target:{dataset:{path}}})=>{console.log(path)};
+
+// for (const btn of btns) {
+//   btn.addEventListener('click', btnHandler); 
+    
+// }
+// const btns = document.querySelectorAll('button');
+
+// const btnHandler = ({
+//   target:{
+//     dataset:{color},
+//     parentNode: parentRoot
+//   }
+// })=>{
+//   parentRoot.style.backgroundColor=color
+// }
+
+// const handler = (e)=>{console.log(e)}
+
+
+// for (const btn of btns) {
+//   btn.addEventListener('click', btnHandler);
+//   btn.addEventListener('click', handler);
+// }    
+
+const btns = document.querySelectorAll('button');
+
+const btnHandler = ({
+    target:{
+      dataset:{color},
+      parentNode: parentRoot
+    }
+  })=>{
+    parentRoot.style.backgroundColor=color
+  }
+
+const handler = (e)=>{
+  e.cancelBubble = true;
+  e.stopPropagation();
+  console.group();
+  console.dir(e.target);
+  console.dir(e.currnetTarget);
+  console.groupEnd();
+  }
+
+  for (const btn of btns) {
+    btn.addEventListener('click', btnHandler);
+    btn.addEventListener('click', handler);
+    btn.addEventListener('click', ()=>{console.log(123)}, {capture:true, once:true});
+    
+  }
+
+  document.getElementById('root').addEventListener('click', handler);
+  document.body.addEventListener('click', handler);
+
+  document.addEventListener('click', handler,true);
+  window.addEventListener('click', handler, true);
+
+   
