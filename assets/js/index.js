b391f9ef7c54f52c4bf51bf3f08fdbf34a2f87b0
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-11-01 10:04:49 +0200

    Refactoring start

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,22 +11,6 @@ function createActorCards(actor){
   const container = document.createElement('article');
   container.classList.add('cardContainer');
 
-  const imgWrapper = document.createElement('div');
-  imgWrapper.classList.add('cardImageWrapper');
-
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(actor.name[0] || 'xyz'));
-  initials.style.backgroundColor=stringToColour(actor.name || '')
-
-  const img = document.createElement('img');
-  img.classList.add('cardImage');
-  img.setAttribute('src', actor.photo);
-  img.setAttribute('alt', actor.name);
-  img.addEventListener('error', handleImageError);
-
-  imgWrapper.append(initials, img);
-
   const name = document.createElement('h2');
   name.classList.add('cardName');
   //name.textContent=actor.name
@@ -36,7 +20,7 @@ function createActorCards(actor){
   description.classList.add('cardDescription');
   description.append(document.createTextNode(actor.birthdate || 'unknown'))
 
-  container.append(imgWrapper, name, description);
+  container.append(createImageWrapper(actor), name, description);
   card.append(container);
   return card;
 }
@@ -45,9 +29,42 @@ function createActorCards(actor){
 // }
 cardsContainer.append(...HTMLElements);
 
+function createImageWrapper(actor){
+  const {id, name}=actor;
+  const imgWrapper = document.createElement('div');
+  imgWrapper.setAttribute('id', `wrapper${id}`);
+  imgWrapper.classList.add('cardImageWrapper');
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(name[0] || 'xyz'));
+  initials.style.backgroundColor=stringToColour(name || '')
+
+  imgWrapper.append(initials, createImage(actor));
+  return imgWrapper;
+}
+
+function createImage({photo,name, id}){
+  const img = document.createElement('img');
+  img.classList.add('cardImage');
+  img.setAttribute('src', photo);
+  img.setAttribute('alt', name);
+  img.dataset.id=id;
+  img.addEventListener('error', handleImageError);
+  img.addEventListener('load', handleImageLoad);
+  return img;
+}
+
 /*handle */
 function handleImageError({target}){
     target.remove();
+}
+function handleImageLoad({
+  target,
+  target:{
+    dataset:{id}
+  }}){
+  document.getElementById(`wrapper${id}`).append(target)
 }
 
 /*utilities*/
