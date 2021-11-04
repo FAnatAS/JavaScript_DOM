9657e64fecc82d7791bfd841d3b641707b5ff814
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-11-04 11:46:40 +0200

    Add new Promise _own

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,26 +16,70 @@
 //   }, 500)
 // }
 
-const user={
-  name:'Elon',
-  sname:'Musk',
-  age:50,
-  getFullName(){
-    return`${this.name} ${this.sname}`;
-  },
-  children:['one', 'two'],
-  isAdult: true,
-  hasPet: undefined,
-  isUkraine: null,
-  [Symbol('test')]:123,
-  friends:{
-    friend1: 'Tom',
-    friend2: 'Tim',
-  },
-}
-console.log(user);
-const serializedUser = JSON.stringify(user);
-console.log(serializedUser);
+//-------------04-11-2021------------------
 
-const deSerializedUser = JSON.parse(serializedUser)
-console.log(deSerializedUser);
+// const user={
+//   name:'Elon',
+//   sname:'Musk',
+//   age:50,
+//   getFullName(){
+//     return`${this.name} ${this.sname}`;
+//   },
+//   children:['one', 'two'],
+//   isAdult: true,
+//   hasPet: undefined,
+//   isUkraine: null,
+//   [Symbol('test')]:123,
+//   friends:{
+//     friend1: 'Tom',
+//     friend2: 'Tim',
+//   },
+// }
+// console.log(user);
+// const serializedUser = JSON.stringify(user);
+// console.log(serializedUser);
+
+// const deSerializedUser = JSON.parse(serializedUser)
+// console.log(deSerializedUser);
+
+
+// let i=0;
+// function f(){
+//   console.log(i++)
+//   f();
+//   // setTimeout(()=>{
+//   //   f();
+//   // },0)
+// }
+
+// f()
+
+//const promise = fetch('./assets/js/data.json');
+
+// promise.then((response)=>{
+//   console.log('ok')
+//   console.log(response)
+//   const jsonPromise = response.json()
+//   jsonPromise.then((data)=>{
+//     console.table(data)
+//   })
+// })
+
+// console.log(promise)
+
+fetch('./assets/js/data.json')
+.then((response)=>response.json())
+.then((data)=>{
+  //console.table(data)
+  data.forEach((user)=>{
+    console.log(user.name);
+   });
+})
+.catch((error)=>{console.log(error);})
+.finally(()=>{console.log('the end')})
+
+const myFirstPromise = new Promise (executor);
+
+function executor(resolve, reject){
+  Math.random()>0.5?resolve():reject();
+}
