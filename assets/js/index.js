d56a3bf118ed10cb9ac01d3a14e0f614e1621b41
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-11-04 09:50:32 +0200

    Add JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,41 @@
 'use strict'
 
-// const btn = document.getElementById('btn');
-// btn.addEventListener('click', btnHandler);
-// function btnHandler(e){
+// // const btn = document.getElementById('btn');
+// // btn.addEventListener('click', btnHandler);
+// // function btnHandler(e){
 
+// // }
+// counter()
+// function counter(){
+//   let i=1;
+//   const id = setInterval(()=>{
+// console.log(i++)
+// if(i>10){
+//   clearInterval(id);
 // }
-counter()
-function counter(){
-  let i=1;
-  const id = setInterval(()=>{
-console.log(i++)
-if(i>10){
-  clearInterval(id);
-}
-  }, 500)
-}
+//   }, 500)
+// }
 
+const user={
+  name:'Elon',
+  sname:'Musk',
+  age:50,
+  getFullName(){
+    return`${this.name} ${this.sname}`;
+  },
+  children:['one', 'two'],
+  isAdult: true,
+  hasPet: undefined,
+  isUkraine: null,
+  [Symbol('test')]:123,
+  friends:{
+    friend1: 'Tom',
+    friend2: 'Tim',
+  },
+}
+console.log(user);
+const serializedUser = JSON.stringify(user);
+console.log(serializedUser);
+
+const deSerializedUser = JSON.parse(serializedUser)
+console.log(deSerializedUser);
