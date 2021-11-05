23210af4e535cee2425c4c394aa14074f527423d
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-11-05 11:03:51 +0200

    Promise_0511

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,23 +84,24 @@
 //   Math.random()>0.5?resolve():reject();
 // }
 
-function loadImage(path){
-  const image=new Image();
-  image.src = path;
-  return new Promise((resolve, reject)=>{
-    image.addEventListener('load', ()=>{
-      resolve(image);
-    })
-    image.addEventListener('error', ()=>{
-      reject(new Error('Invalid path'))
-    })
-  });
-}
-loadImage('https://friendofthesea.org/public/news/blue-ocean-28668-2560x1600.jpg')
-.then((elem)=>{
-  document.body.prepend(elem);
+// function loadImage(path){
+//   const image=new Image();
+//   image.src = path;
+//   return new Promise((resolve, reject)=>{
+//     image.addEventListener('load', ()=>{
+//       resolve(image);
+//     })
+//     image.addEventListener('error', ()=>{
+//       reject(new Error('Invalid path'))
+//     })
+//   });
+// }
+// loadImage('https://friendofthesea.org/public/news/blue-ocean-28668-2560x1600.jpg')
+// .then((elem)=>{
+//   document.body.prepend(elem);
 
-})
-.catch((err)=>{
-  console.log(err)
-})
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
+
